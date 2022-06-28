847f5f6d2b172574479e60cdbb8e62c3d3850a8d
luis ponce-alvarez <luisponce-alvarez@luiss-Mac-mini.local>
2022-06-28 16:05:11 -0600

    el cambio

--- ms_mint_conc/SessionState.py
+++ ms_mint_conc/SessionState.py
@@ -16,7 +16,7 @@ result:
 'Mary'
 """
 try:
-    import streamlit.ReportThread as ReportThread
+    import streamlit as ReportThread
     from streamlit.server.Server import Server
 except Exception:
     # Streamlit >= 0.65.0
