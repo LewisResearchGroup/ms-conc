9b4d2c88004fed7392fe7553d7267ab6f3ba2de2
luis ponce-alvarez <luisponce-alvarez@luiss-Mac-mini.local>
2022-06-28 16:06:48 -0600

    el cambio

--- ms_mint_conc/SessionState.py
+++ ms_mint_conc/SessionState.py
@@ -20,7 +20,7 @@ try:
     from streamlit.server.Server import Server
 except Exception:
     # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportThread
+    import streamlit as ReportThread
     from streamlit.server.server import Server
 
 
