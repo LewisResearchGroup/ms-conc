dd8006fe87696f0e99cce9c765e09a154eb2ca12
luis-ponce <57267049+luis-ponce@users.noreply.github.com>
2024-02-20 17:45:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,6 +153,7 @@ try:
         if '.csv' in std_info.name:
             st.session_state.std_information = pd.read_csv(std_info)
         if '.xlsx' in std_info.name:
+            st.write('im here')
             st.session_state.std_information = pd.read_excel(std_info)
             
         if 'unit' in st.session_state.std_information.columns:
