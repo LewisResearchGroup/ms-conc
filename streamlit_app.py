c474549dd1fb7c3c0c0da56b75b94d1e0b23ce70
luis-ponce <57267049+luis-ponce@users.noreply.github.com>
2024-02-21 12:43:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,6 @@ try:
         if '.csv' in std_info.name:
             st.session_state.std_information = pd.read_csv(std_info)
         if '.xlsx' in std_info.name:
-            st.write(std_info.name)
             st.session_state.std_information = pd.read_excel(std_info)
             st.write(pd.read_excel(std_info))
 
