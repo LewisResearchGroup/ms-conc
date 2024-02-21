049b73adace91ee1fd952131238d9ec06ccf1ff4
luis-ponce <57267049+luis-ponce@users.noreply.github.com>
2024-02-21 14:18:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -392,7 +392,11 @@ try:
             st.write(dat[dat.columns[:-2]])
             st.session_state.u = st.session_state.units.unit[st.session_state.units.peak_label == st.session_state.cp].iloc[0]
             st.session_state.xlabel = st.text_input("Please enter the x-label", st.session_state.cp + ' concentration (' + st.session_state.u + ')')
-            st.session_state.ylabel = st.text_input("Please enter the y-label", st.session_state.cp + ' intensity (AU)')
+
+            if st.session_state.internal == 'on':
+                st.session_state.ylabel = st.text_input("Please enter the y-label", st.session_state.cp + ' response ratio')
+            else:
+                st.session_state.ylabel = st.text_input("Please enter the y-label", st.session_state.cp + ' intensity (AU)')
         
         
             
