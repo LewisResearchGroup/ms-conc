5bcec1e30d363f919661e06fda44d18ab8dc8c63
luis-ponce <57267049+luis-ponce@users.noreply.github.com>
2024-02-21 15:18:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,8 +181,18 @@ try:
             st.session_state.internal = 'off'
             
     st.write('## Your standards concentrations file:')
-    st.button('Click me', on_click = st.dataframe(st.session_state.std_information))
+    
     # st.dataframe(st.session_state.std_information)
+
+    if 'button1' not in st.session_state:
+    st.session_state.button1 = False
+    def click_button1():
+        st.session_state.button1 = not st.session_state.button1
+    st.button('Click me', on_click=click_button1)
+    if st.session_state.button1:
+        # The message and nested widget will remain on the page
+        st.dataframe(st.session_state.std_information)
+else:
     # st.write(st.session_state.std_information)
 except:
     st.write('## Please upload your standards concentrations file to start')
