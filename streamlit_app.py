34c2b9486bbdbef6c16a1f01b72b0c94fe6637db
luis-ponce <57267049+luis-ponce@users.noreply.github.com>
2025-04-16 16:27:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -293,7 +293,7 @@ try:
                 
             # st.write(st.session_state.raw_results)
             st.session_state.std_results = cc.setting_from_stdinfo(st.session_state.std_information, st.session_state.raw_results)
-            # st.write(st.session_state.std_results)
+            st.write(st.session_state.std_results)
             st.session_state.std_results.sort_values(by = ['peak_label','STD_CONC', st.session_state.by_ ], inplace = True)
             
         except:
